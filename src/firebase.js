e7da332c7d9ad7793bca31e711c2a24a1c4cd9d1
machhishubham63-gif <machhishubham63@gmail.com>
2026-03-03 17:25:12 +0530

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDPxWCiZJLtfb9uC5JM9oymGbiU2nB_aiI",
+  authDomain: "travel-log-e100c.firebaseapp.com",
+  projectId: "travel-log-e100c",
+  storageBucket: "travel-log-e100c.firebasestorage.app",
+  messagingSenderId: "70640769799",
+  appId: "1:70640769799:web:75c41a674290efffc543a2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
