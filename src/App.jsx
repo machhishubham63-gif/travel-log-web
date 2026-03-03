462d8224e8184ddb16029bc77df41c7dcbb346fb
machhishubham63-gif <machhishubham63@gmail.com>
2026-03-03 20:11:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,30 @@
 import { useEffect, useState } from "react";
 import { auth } from "./firebase";
-import { onAuthStateChanged, signOut } from "firebase/auth";
-
+import { onAuthStateChanged } from "firebase/auth";
 import Login from "./Login";
-import TravelForm from "./TravelForm";
-import TravelList from "./TravelList";
-
-function Dashboard() {
-  <TravelList user={auth.currentUser} 
-  key={refreshKey} />
-  return (
-    <div
-      style={{
-        maxWidth: "500px",
-        margin: "auto",
-        padding: "20px",
-        fontFamily: "sans-serif"
-      }}
-    >
-      <h1 style={{ textAlign: "center" }}>My Travel Log</h1>
-
-      <button
-        onClick={() => signOut(auth)}
-        style={{
-          width: "100%",
-          padding: "10px",
-          marginBottom: "20px"
-        }}
-      >
-        Logout
-      </button>
-
-      <TravelForm refresh={() => setRefreshKey(old => old + 1)} />
-      <TravelList key={refreshKey} />
-    </div>
-  );
-}
+import Dashboard from "./Dashboard";
 
 function App() {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
+      setLoading(false);
     });
 
     return () => unsubscribe();
   }, []);
 
-  return (
-    <div>
-      {user ? <Dashboard /> : <Login />}
-    </div>
+  if (loading) {
+    return <div style={{ padding: "20px" }}>Loading...</div>;
+  }
+
+  return user ? (
+    <Dashboard user={user} />
+  ) : (
+    <Login />
   );
 }
 
