8899c08197d4f961c1ed55cf232040efcd83edbf
machhishubham63-gif <machhishubham63@gmail.com>
2026-03-03 20:13:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react";
 import { auth } from "./firebase";
 import { onAuthStateChanged } from "firebase/auth";
 import Login from "./Login";
-import Dashboard from "./Dashboard";
+import TravelForm from "./TravelForm";
+import TravelList from "./TravelList";
 
 function App() {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [refreshKey, setRefreshKey] = useState(0);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
@@ -21,10 +23,21 @@ function App() {
     return <div style={{ padding: "20px" }}>Loading...</div>;
   }
 
-  return user ? (
-    <Dashboard user={user} />
-  ) : (
-    <Login />
+  if (!user) {
+    return <Login />;
+  }
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Travel Log Dashboard</h2>
+
+      <TravelForm
+        user={user}
+        onAdd={() => setRefreshKey((prev) => prev + 1)}
+      />
+
+      <TravelList user={user} key={refreshKey} />
+    </div>
   );
 }
 
