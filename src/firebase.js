6aadb2eb725a05fa666f45ca31cdee5049584e4f
machhishubham63-gif <machhishubham63@gmail.com>
2026-03-03 17:33:01 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDPxWCiZJLtfb9uC5JM9oymGbiU2nB_aiI",
-  authDomain: "travel-log-e100c.firebaseapp.com",
-  projectId: "travel-log-e100c",
-  storageBucket: "travel-log-e100c.firebasestorage.app",
-  messagingSenderId: "70640769799",
-  appId: "1:70640769799:web:75c41a674290efffc543a2"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_ID",
+  appId: "YOUR_APP_ID"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
