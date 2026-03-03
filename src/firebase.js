59a829a990cf636928f39f5a32183eab0032bc5e
machhishubham63-gif <machhishubham63@gmail.com>
2026-03-03 17:49:52 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,8 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDPxWCiZJLtfb9uC5JM9oymGbiU2nB_aiI",
+  authDomain: "travel-log-e100c.firebaseapp.com",
+  projectId: "travel-log-e100c",
+  storageBucket: "travel-log-e100c.firebasestorage.app",
+  messagingSenderId: "70640769799",
+  appId: "1:70640769799:web:75c41a674290efffc543a2"
 };
-
-const app = initializeApp(firebaseConfig);
-
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { auth, db };
