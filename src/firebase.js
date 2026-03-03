6befa9a71bbd7cbd0287ab4c74d0f72231d1b3c6
machhishubham63-gif <machhishubham63@gmail.com>
2026-03-03 17:52:30 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDPxWCiZJLtfb9uC5JM9oymGbiU2nB_aiI",
   authDomain: "travel-log-e100c.firebaseapp.com",
@@ -6,3 +10,10 @@ const firebaseConfig = {
   messagingSenderId: "70640769799",
   appId: "1:70640769799:web:75c41a674290efffc543a2"
 };
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
